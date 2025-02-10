e5461364d73d38bdddb103909529add26b024be7
kevin <120391759+kevi07@users.noreply.github.com>
2025-02-10 17:44:43 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
   "https://main.d1sj7cd70hlter.amplifyapp.com",
   "https://expense-tracker-app-three-beryl.vercel.app",
   "http://localhost:3000",
+  "https://trackit-frontend-1die.onrender.com",
   
   // add more origins as needed
 ];
